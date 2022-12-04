0fcaa7b6ed9aaf152769d39709b7c7d69938c515
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2022-12-04 18:41:43 +0800

    Use implementation from faulty commit

--- experiments/data/objects.pxd
+++ experiments/data/objects.pxd
@@ -1,10 +1,9 @@
 from experiments.data.rust.core cimport UUID4_t
 
 cdef class UUID4:
-    cdef UUID4_t _mem
+    cdef UUID4_t _uuid4
 
     cdef readonly str value
     """The UUID4 value.\n\n:returns: `str`"""
 
-    cdef str to_str(self)
-
+    cdef UUID4_t _uuid4_from_pystr(self, str value) except *
--- experiments/data/objects.pyx
+++ experiments/data/objects.pyx
@@ -6,26 +6,27 @@ from experiments.data.rust.core cimport uuid4_new
 from experiments.data.rust.core cimport uuid4_to_pystr
 
 cdef class UUID4:
-    def __init__(self, str value = None):
+    def __init__(self, str value=None):
         if value is None:
             # Create a new UUID4 from Rust
-            self._mem = uuid4_new()  # `UUID4_t` owned from Rust
+            self._uuid4 = uuid4_new()  # `UUID4_t` owned from Rust
+            self.value = <str>uuid4_to_pystr(&self._uuid4)  # `PyUnicode` owned from Rust
         else:
-            # `value` borrowed by Rust, `UUID4_t` owned from Rust
-            self._mem = uuid4_from_pystr(<PyObject *>value)
+            self._uuid4 = self._uuid4_from_pystr(value)
+            self.value = value
 
-    cdef str to_str(self):
-        return <str>uuid4_to_pystr(&self._mem)
+    cdef UUID4_t _uuid4_from_pystr(self, str value) except *:
+        return uuid4_from_pystr(<PyObject *>value)  # `value` borrowed by Rust, `UUID4_t` owned from Rust
 
     def __del__(self) -> None:
-        if self._mem.value != NULL:
-            uuid4_free(self._mem)  # `self._uuid4` moved to Rust (then dropped)
+        uuid4_free(self._uuid4)  # `self._uuid4` moved to Rust (then dropped)
 
     def __getstate__(self):
-        return self.to_str()
+        return self.value
 
     def __setstate__(self, state):
-        self._mem = uuid4_from_pystr(<PyObject *>state)
+        self._uuid4 = self._uuid4_from_pystr(state)
+        self.value = state
 
     def __eq__(self, UUID4 other) -> bool:
         return self.value == other.value
@@ -34,7 +35,7 @@ cdef class UUID4:
         return hash(self.value)
 
     def __str__(self) -> str:
-        return self.to_str()
+        return self.value
 
     def __repr__(self) -> str:
         return f"{type(self).__name__}('{self.value}')"
