fe0151cc6f2f7fd02cc9283007d8f24da1d2c411
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2023-01-28 15:41:44 +0800

    Add tests

--- experiments/core/src/lib.rs
+++ experiments/core/src/lib.rs
@@ -1,3 +1,5 @@
+#[repr(C)]
+#[derive(Clone, Hash, PartialEq, Eq, Debug)]
 pub struct OrderInitialized {
     pub trader_id: String,
     pub strategy_id: String,
@@ -7,10 +9,38 @@ pub struct OrderInitialized {
     pub trailing_offset_type: Option<u64>,
 }
 
+#[repr(C)]
+#[derive(Clone, Hash, PartialEq, Eq, Debug)]
 pub struct OrderExpired {
     pub trader_id: String,
     pub strategy_id: String,
     pub ts_event: u64,
     pub ts_init: u64,
     pub reconcilliation: u64,
+}
+
+mod tests {
+    use crate::{OrderExpired, OrderInitialized};
+
+    #[test]
+    fn build_expired_order() {
+        let mut expired = OrderExpired {
+            trader_id: "hi".to_string(),
+            strategy_id: "bye".to_string(),
+            ts_event: 1,
+            ts_init: 1,
+            reconcilliation: 2,
+        };
+    }
+
+    fn build_initialized_order() {
+        let mut init = OrderInitialized {
+            trader_id: "hi".to_string(),
+            strategy_id: "bye".to_string(),
+            ts_event: 1,
+            ts_init: 1,
+            trailing_offset: Some(2),
+            trailing_offset_type: Some(3),
+        };
+    }
 }
