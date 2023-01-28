6a0d119a74c93cd2c68ff128f185227d7a1216a8
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2023-01-28 15:37:09 +0800

    Initial order structs

--- experiments/core/src/lib.rs
+++ experiments/core/src/lib.rs
@@ -1,96 +1,16 @@
-use std::collections::hash_map::DefaultHasher;
-use std::fmt::{Debug, Display, Formatter, Result};
-use std::hash::{Hash, Hasher};
-use std::rc::Rc;
-
-use pyo3::types::{PyString, PyUnicode};
-use pyo3::{ffi, FromPyPointer, IntoPyPointer, Py, Python};
-
-use uuid::Uuid;
-
-#[repr(C)]
-#[derive(Clone, Hash, PartialEq, Eq, Debug)]
-#[allow(clippy::box_collection)] // C ABI compatibility
-#[allow(clippy::redundant_allocation)] // C ABI compatibility
-pub struct UUID4 {
-    pub value: Box<Rc<String>>,
+pub struct OrderInitialized {
+    pub trader_id: String,
+    pub strategy_id: String,
+    pub ts_event: u64,
+    pub ts_init: u64,
+    pub trailing_offset: Option<u64>,
+    pub trailing_offset_type: Option<u64>,
 }
 
-impl UUID4 {
-    pub fn new() -> UUID4 {
-        let uuid = Uuid::new_v4();
-        UUID4 {
-            value: Box::new(Rc::new(uuid.to_string())),
-        }
-    }
-}
-
-impl From<&str> for UUID4 {
-    fn from(s: &str) -> Self {
-        let uuid = Uuid::try_parse(s).expect("invalid UUID string");
-        UUID4 {
-            value: Box::new(Rc::new(uuid.to_string())),
-        }
-    }
-}
-impl Display for UUID4 {
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        write!(f, "{}", self.value)
-    }
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// C API
-////////////////////////////////////////////////////////////////////////////////
-#[no_mangle]
-pub extern "C" fn uuid4_new() -> UUID4 {
-    UUID4::new()
-}
-
-#[no_mangle]
-pub extern "C" fn uuid4_clone(uuid4: &UUID4) -> UUID4 {
-    uuid4.clone()
-}
-
-#[no_mangle]
-pub extern "C" fn uuid4_free(uuid4: UUID4) {
-    drop(uuid4); // Memory freed here
-}
-
-/// Returns a `UUID4` from a valid Python object pointer.
-///
-/// # Safety
-/// - Assumes `ptr` is borrowed from a valid Python UTF-8 `str`.
-#[no_mangle]
-pub unsafe extern "C" fn uuid4_from_pystr(ptr: *mut ffi::PyObject) -> UUID4 {
-    UUID4::from(
-        { Python::with_gil(|py| PyString::from_borrowed_ptr(py, ptr).to_string()) }.as_str(),
-    )
-}
-
-/// Returns a pointer to a valid Python UTF-8 string.
-///
-/// # Safety
-/// - Assumes that since the data is originating from Rust, the GIL does not need
-/// to be acquired.
-/// - Assumes you are immediately returning this pointer to Python.
-#[no_mangle]
-pub unsafe extern "C" fn uuid4_to_pystr(uuid: &UUID4) -> *mut ffi::PyObject {
-    let s = uuid.value.as_str();
-    Python::with_gil(|py| {
-        let pystr: Py<PyString> = PyString::new(py, s).into();
-        pystr.into_ptr()
-    })
-}
-
-#[no_mangle]
-pub extern "C" fn uuid4_eq(lhs: &UUID4, rhs: &UUID4) -> u8 {
-    (lhs == rhs) as u8
-}
-
-#[no_mangle]
-pub extern "C" fn uuid4_hash(uuid: &UUID4) -> u64 {
-    let mut h = DefaultHasher::new();
-    uuid.hash(&mut h);
-    h.finish()
+pub struct OrderExpired {
+    pub trader_id: String,
+    pub strategy_id: String,
+    pub ts_event: u64,
+    pub ts_init: u64,
+    pub reconcilliation: u64,
 }
--- experiments/data/rust/core.pxd
+++ experiments/data/rust/core.pxd
@@ -4,33 +4,4 @@ from cpython.object cimport PyObject
 from libc.stdint cimport uintptr_t, uint8_t, uint64_t
 
 cdef extern from "core.h":
-
-    cdef struct Rc_String:
-        pass
-
-    cdef struct UUID4_t:
-        Rc_String *value;
-
-    UUID4_t uuid4_new();
-
-    UUID4_t uuid4_clone(const UUID4_t *uuid4);
-
-    void uuid4_free(UUID4_t uuid4);
-
-    # Returns a `UUID4` from a valid Python object pointer.
-    #
-    # # Safety
-    # - Assumes `ptr` is borrowed from a valid Python UTF-8 `str`.
-    UUID4_t uuid4_from_pystr(PyObject *ptr);
-
-    # Returns a pointer to a valid Python UTF-8 string.
-    #
-    # # Safety
-    # - Assumes that since the data is originating from Rust, the GIL does not need
-    # to be acquired.
-    # - Assumes you are immediately returning this pointer to Python.
-    PyObject *uuid4_to_pystr(const UUID4_t *uuid);
-
-    uint8_t uuid4_eq(const UUID4_t *lhs, const UUID4_t *rhs);
-
-    uint64_t uuid4_hash(const UUID4_t *uuid);
+    pass
