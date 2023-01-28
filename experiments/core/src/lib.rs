3c5260283f2a6fc5c9e43ee734b76dd66bbda407
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2023-01-28 19:12:00 +0800

    Derive builders

--- experiments/core/src/lib.rs
+++ experiments/core/src/lib.rs
@@ -1,7 +1,8 @@
+use derive_builder::{self, Builder};
 use std::ops::{Deref, DerefMut};
 
 #[repr(C)]
-#[derive(Clone, Hash, PartialEq, Eq, Debug)]
+#[derive(Clone, Hash, PartialEq, Eq, Debug, Builder)]
 pub struct Order {
     pub trader_id: String,
     pub strategy_id: String,
@@ -28,7 +29,7 @@ macro_rules! impl_derefs_for_order {
 }
 
 #[repr(C)]
-#[derive(Clone, Hash, PartialEq, Eq, Debug)]
+#[derive(Clone, Hash, PartialEq, Eq, Debug, Builder)]
 pub struct OrderInitialized {
     pub order: Order,
     pub trailing_offset: Option<u64>,
@@ -36,7 +37,7 @@ pub struct OrderInitialized {
 }
 
 #[repr(C)]
-#[derive(Clone, Hash, PartialEq, Eq, Debug)]
+#[derive(Clone, Hash, PartialEq, Eq, Debug, Builder)]
 pub struct OrderExpired {
     pub order: Order,
     pub reconcilliation: u64,
@@ -45,55 +46,61 @@ pub struct OrderExpired {
 impl_derefs_for_order!(OrderInitialized);
 impl_derefs_for_order!(OrderExpired);
 
-
 mod tests {
-    use crate::{Order, OrderExpired, OrderInitialized};
+    use crate::{
+        Order, OrderBuilder, OrderExpired, OrderExpiredBuilder, OrderInitialized,
+        OrderInitializedBuilder,
+    };
 
     #[test]
     fn build_expired_order() {
-        let mut expired = OrderExpired {
-            order: Order {
-                trader_id: "hi".to_string(),
-                strategy_id: "bye".to_string(),
-                ts_event: 1,
-                ts_init: 1,
-            },
-            reconcilliation: 2,
+        let order = Order {
+            trader_id: "hi".to_string(),
+            strategy_id: "bye".to_string(),
+            ts_event: 1,
+            ts_init: 1,
         };
+        let mut builder = OrderExpiredBuilder::default();
+        builder.order(order).reconcilliation(2);
+        let expired = builder.build().unwrap();
 
         assert_eq!(expired.ts_event, 1);
         assert_eq!(expired.trader_id, "hi");
         assert_eq!(expired.reconcilliation, 2);
-        
+
         // mutate through order attribute only
+        builder.reconcilliation(3);
+        let mut expired = builder.build().unwrap();
         expired.trader_id = "oh no".to_string();
-        expired.reconcilliation = 3;
-        
+
         assert_eq!(expired.trader_id, "oh no");
         assert_eq!(expired.reconcilliation, 3);
     }
 
     #[test]
     fn build_initialized_order() {
-        let mut init = OrderInitialized {
-            order: Order {
-                trader_id: "hi".to_string(),
-                strategy_id: "bye".to_string(),
-                ts_event: 1,
-                ts_init: 1,
-            },
-            trailing_offset: Some(2),
-            trailing_offset_type: Some(3),
+        let order = Order {
+            trader_id: "hi".to_string(),
+            strategy_id: "bye".to_string(),
+            ts_event: 1,
+            ts_init: 1,
         };
+        let mut builder = OrderInitializedBuilder::default();
+        builder
+            .order(order)
+            .trailing_offset(Some(2))
+            .trailing_offset_type(Some(3));
+        let init = builder.build().unwrap();
 
         assert_eq!(init.ts_event, 1);
         assert_eq!(init.trader_id, "hi");
         assert_eq!(init.trailing_offset, Some(2));
 
         // mutate through order attribute only
+        builder.trailing_offset(Some(7));
+        let mut init = builder.build().unwrap();
         init.trader_id = "oh no".to_string();
-        init.trailing_offset = Some(7);
-        
+
         assert_eq!(init.trader_id, "oh no");
         assert_eq!(init.trailing_offset, Some(7));
     }
