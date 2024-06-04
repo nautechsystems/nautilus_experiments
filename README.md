a4ceb950de3b4bbc43ec82b64ee1495d077f5116
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2024-06-04 22:42:09 +0530

    Add full file scan script

--- README.md
+++ README.md
@@ -8,10 +8,10 @@
 The binary reads the file and prints the number of rows from the first row group. We compare different configurations for their time and memory usage. You can run the following queries to get these values.
 
 ```
-/usr/bin/time -v cargo run --release data.parquet order repartition
-/usr/bin/time -v cargo run --release data.parquet order
-/usr/bin/time -v cargo run --release data.parquet 
-/usr/bin/time -v cargo run --release data.parquet repartition
+/usr/bin/time -v cargo run --release --bin single_row_group data.parquet order repartition
+/usr/bin/time -v cargo run --release --bin single_row_group data.parquet order
+/usr/bin/time -v cargo run --release --bin single_row_group data.parquet 
+/usr/bin/time -v cargo run --release --bin single_row_group data.parquet repartition
 ```
 
 These are the values on my system.
@@ -22,3 +22,19 @@ These are the values on my system.
 | true | false | 1.19 | 944 | ✅ |
 | false | false | 0.21 | 45 | ✅ |
 | false | true | 0.33 | 151 | ❌ |
+
+The `full_file` binary reads the whole file and counts the total number of rows.
+
+```
+/usr/bin/time -v cargo run --release --bin full_file data.parquet order repartition
+/usr/bin/time -v cargo run --release --bin full_file data.parquet order
+/usr/bin/time -v cargo run --release --bin full_file data.parquet 
+/usr/bin/time -v cargo run --release --bin full_file data.parquet repartition
+```
+
+| order | repartition | wall time (s) | memory (mb) | read sorted order |
+| -- | -- | -- | -- | -- |
+| true | true | 1.24 | 654 | ✅ |
+| true | false | 1.65 | 944 | ✅ |
+| false | false | 0.55 | 45 | ✅ |
+| false | true | 0.41 | 151 | ❌ |
---
+++ src/bin/full_file.rs
@@ -0,0 +1,52 @@
+use std::env;
+
+use datafusion::execution::{context::SessionContext, options::ParquetReadOptions};
+use datafusion::logical_expr::expr::Sort;
+use datafusion::prelude::*;
+use futures::StreamExt;
+
+#[tokio::main]
+async fn main() {
+    // Read file_path from command-line arguments
+    let file_path = env::args().nth(1).expect("Missing file_path argument");
+    // Set repartitioning option in session configuration
+    let repartition_file_scan = if env::args().find(|v| v == "repartition").is_some() {
+        "true"
+    } else {
+        "false"
+    };
+    // Set sql query ordering clause
+    let sql_query = if env::args().find(|v| v == "order").is_some() {
+        "SELECT * FROM data ORDER BY ts_init"
+    } else {
+        "SELECT * FROM data"
+    };
+
+    let session_cfg = SessionConfig::new().set_str(
+        "datafusion.optimizer.repartition_file_scans",
+        repartition_file_scan,
+    );
+    let session_ctx = SessionContext::new_with_config(session_cfg);
+    let parquet_options = ParquetReadOptions::<'_> {
+        skip_metadata: Some(false),
+        file_sort_order: vec![vec![Expr::Sort(Sort {
+            expr: Box::new(col("ts_init")),
+            asc: true,
+            nulls_first: true,
+        })]],
+        ..Default::default()
+    };
+    session_ctx
+        .register_parquet("data", &file_path, parquet_options)
+        .await
+        .unwrap();
+    let query = session_ctx.sql(sql_query).await.unwrap();
+    let batch_stream = query.execute_stream().await.unwrap();
+
+    let row_count = batch_stream
+        .fold(0, |acc, batch| async move {
+            acc + batch.map(|batch| batch.num_rows()).unwrap_or(0)
+        })
+        .await;
+    println!("Total rows: {}", row_count);
+}
