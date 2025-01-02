7fc3b1e1c2fae9ddd95e402de7709c491394f4f4
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2025-01-02 15:17:09 +0530

    MRE

--- experiments/core/src/lib.rs
+++ experiments/core/src/lib.rs
@@ -1,26 +1,8 @@
-use std::ffi::{c_char, CStr, CString};
-use std::fmt::{Display, Formatter};
-use ustr::Ustr;
-
-#[repr(C)]
-#[derive(Clone, Copy, Hash, PartialEq, Eq, Debug)]
-pub struct TradeId {
-    value: Ustr,
-}
-
-impl Display for TradeId {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.value)
-    }
-}
-
 /// Represents a single quote tick in a financial market.
 #[repr(C)]
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub struct TradeTick {
-    pub trade_id: TradeId,
-    pub ts_event: u64,
-    pub ts_init: u64,
+    pub ts_event: u128,
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -28,25 +10,13 @@ pub struct TradeTick {
 ////////////////////////////////////////////////////////////////////////////////
 
 #[no_mangle]
-pub extern "C" fn trade_tick_new(trade_id: TradeId, ts_event: u64, ts_init: u64) -> TradeTick {
-    TradeTick {
-        trade_id,
-        ts_event,
-        ts_init,
-    }
+#[allow(improper_ctypes_definitions)]
+pub extern "C" fn trade_tick_new(ts_event: u128) -> TradeTick {
+    TradeTick { ts_event }
 }
 
 #[no_mangle]
 pub extern "C" fn trade_tick_eq(lhs: &TradeTick, rhs: &TradeTick) -> u8 {
     assert_eq!(lhs.ts_event, rhs.ts_event);
-    assert_eq!(lhs.ts_init, rhs.ts_init);
-    assert_eq!(lhs.trade_id, rhs.trade_id);
     u8::from(lhs == rhs)
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn trade_id_new(ptr: *const c_char) -> TradeId {
-    TradeId {
-        value: Ustr::from(CStr::from_ptr(ptr).to_str().expect("CStr::from_ptr failed")),
-    }
 }
--- experiments/data/objects.pxd
+++ experiments/data/objects.pxd
@@ -1,14 +1,6 @@
-from experiments.data.rust.core cimport TradeId_t
 from experiments.data.rust.core cimport TradeTick_t
 
 from libc.stdint cimport uint64_t, uint8_t
-
-cdef class TradeId:
-    cdef TradeId_t _mem
-    cdef str to_str(self)
-
-    @staticmethod
-    cdef TradeId from_mem_c(TradeId_t mem)
 
 cdef class TradeTick:
     cdef TradeTick_t _mem
